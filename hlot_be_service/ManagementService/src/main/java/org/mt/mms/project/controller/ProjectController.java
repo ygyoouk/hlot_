d84dc9e3dd5e968bd0e11a3ea4a2980ae7878d28
choisungju <tjdwn0320@naver.com>
2024-04-29 15:47:35 +0900

    test

--- hlot_be_service/ManagementService/src/main/java/org/mt/mms/project/controller/ProjectController.java
+++ hlot_be_service/ManagementService/src/main/java/org/mt/mms/project/controller/ProjectController.java
@@ -26,7 +26,7 @@ public class ProjectController {
 
     @GetMapping("/project")
     public ResponseEntity<List<ProjectVO>> all() throws Exception{
-
+        log.info("testtesttest");
      log.info("selectProjectList");
      
      /* 프로젝트 리스트 조회*/
